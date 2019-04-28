4b71476f68d9847d14eea86a8bf4c9bef4820894
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-04-29 11:04:32 +1200

    Used a different pdf to html converter

---
+++ cv/config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":9,"title":"","author":"","fileName":"cv.pdf","bounds":[[909,1286],[909,1286],[909,1286],[909,1286],[909,1286],[909,1286],[909,1286],[909,1286],[909,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
